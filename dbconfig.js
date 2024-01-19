439f509a02d39e1c6b1245fa7752d2aa0dad104f
Nightfuury <rupsri1996@gmail.com>
2024-01-20 03:15:50 +0530

    db

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
-    host: "ec2-3-108-54-157.ap-south-1.compute.amazonaws.com",
+    host: "localhost",
     user: "user",
     password: "localhost1",
     database:"MYSQL"
