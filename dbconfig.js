32f12511a11e416e8f6af06f347bf8290dabfbc6
Nightfuury <rupsri1996@gmail.com>
2024-01-21 02:21:58 +0530

    rds

--- dbconfig.js
+++ dbconfig.js
@@ -11,7 +11,8 @@ var db=mysql.createConnection({
     host : "database-bsw.cufs3q03c2kq.ap-south-1.rds.amazonaws.com",
     user : "admin",
     password : "0SfjhwysN8mteoxLnHK6",
-    port : "3306"
+    port : "3306",
+    database:"database-bsw"
 })
 
 
