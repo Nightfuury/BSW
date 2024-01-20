6a73a4025a7c5eee3b74998b9bff4b105011493a
Nightfuury <rupsri1996@gmail.com>
2024-01-21 02:27:02 +0530

    db

--- dbconfig.js
+++ dbconfig.js
@@ -11,8 +11,7 @@ var db=mysql.createConnection({
     host : "database-bsw.cufs3q03c2kq.ap-south-1.rds.amazonaws.com",
     user : "admin",
     password : "0SfjhwysN8mteoxLnHK6",
-    port : "3306",
-    database:"database-bsw"
+    port : "3306"
 })
 
 
